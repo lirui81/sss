e51746c153f154f3b6f9e36e757436248798400f
wangtingting <1244226308@qq.com>
2020-07-12 16:06:39 +0800

    提交人：王婷婷 提交内容：用户登录1.0

--- src/main/java/com/example/sss/service/UserService/UserServiceImpl.java
+++ src/main/java/com/example/sss/service/UserService/UserServiceImpl.java
@@ -1,7 +1,7 @@
-package com.example.sss.UserService;
+package com.example.sss.service.UserService;
 
 import com.example.sss.dao.UserMapper;
-import com.example.sss.model.User;
+import com.example.sss.model.domin.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,4 +18,19 @@ public class UserServiceImpl implements UserService{
         User user = userMapper.selectUserByName("admin");
         return user;
     }
+
+    @Override
+    public User login(User user) {
+        User user1 = new User();
+        user = userMapper.login(user);
+        if(user==null){
+            user1.setStaus(false);
+        }else{
+            user1=user;
+            user1.setStaus(true);
+        }
+        return user1;
+    }
+
+
 }
