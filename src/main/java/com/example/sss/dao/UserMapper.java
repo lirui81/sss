b1d0e1beb51442ca1a4e05ad8d33d5b42b707d81
wangtingting <1244226308@qq.com>
2020-07-13 17:12:16 +0800

    提交人：王婷婷 提交内容：1、修改一些配置2、用户管理列表3、禁用账户登录

--- src/main/java/com/example/sss/dao/UserMapper.java
+++ src/main/java/com/example/sss/dao/UserMapper.java
@@ -3,6 +3,8 @@ package com.example.sss.dao;
 import com.example.sss.model.domin.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  *
  */
@@ -10,4 +12,6 @@ import org.springframework.stereotype.Repository;
 public interface UserMapper {
     User selectUserByName(String name);
     User login(User user);
+    List<User> selectUsersList(User user);
+    void addUser(User user);
 }
